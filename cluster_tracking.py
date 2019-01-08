3600b12c35a70c897e82476d3a6383b87ee82b97
Mahtab Sandhu Sandhu <ironmonger@green.rrc.iiit.ac.in>
2019-01-08 14:18:05 +0530

    initalized tracking and clustering

---
+++ cluster_tracking.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('../pyflow/')
+import pyflow
+import numpy as np
+import cv2
+
+# calculate flow for tracking 
+# assemble them in clusters
+# track it using optical flow
+# single color coding scheme
+# assign reference points to clusters
+
+
+
+if __name__ == "__main__":
+
